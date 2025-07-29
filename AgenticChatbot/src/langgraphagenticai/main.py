e5b64d6da0ae8bd6bbe0fb930e4c70d2fa440ed7
jaivardhan1209 <jaivardhan1209@gmail.com>
2025-07-29 14:15:37 -0400

    push the boiler code

---
+++ AgenticChatbot/src/langgraphagenticai/main.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from src.langgraphagenticai.ui.streamlitui.loadui import LoadStreamlitUI
+#from src.langgraphagenticai.LLMS.groqllm import GroqLLM
+#from src.langgraphagenticai.graph.graph_builder import GraphBuilder
+#from src.langgraphagenticai.ui.streamlitui.display_result import DisplayResultStreamlit
+
+def load_langgraph_agenticai_app():
+    """
+    Loads and runs the LangGraph AgenticAI application with Streamlit UI.
+    This function initializes the UI, handles user input, configures the LLM model,
+    sets up the graph based on the selected use case, and displays the output while 
+    implementing exception handling for robustness.
+
+    """
+
+    ##Load UI
+    ui=LoadStreamlitUI()
+    user_input=ui.load_streamlit_ui()
+
+    if not user_input:
+        st.error("Error: Failed to load user input from the UI.")
+        return
+    
+    user_message = st.chat_input("Enter your message:")
